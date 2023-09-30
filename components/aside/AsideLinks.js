80f15a64d5a3837afd7b2494cdf6387336157616
anochiwaalfred <anochiwaalfred@gmail>
2023-09-30 08:18:12 +0100

    Added Initial Layout

---
+++ components/aside/AsideLinks.js
@@ -0,0 +1,17 @@
+
+import Link from 'next/link'
+import React from 'react'
+
+const AsideLinks = () => {
+  return (
+    <div className="m-3 asideLinks">
+
+    </div>
+  )
+}
+
+export default AsideLinks
+
+// <Link href="#" className="asideLink col-12 pe-5" >
+// <span className='col-12 border m-0 pe-5'>Home</span>
+// </Link> 
